42583ef9c2bdc1b23830f942e1624ddfbbca09de
YudahTech <dammilare70@gmail.com>
2024-07-03 22:18:58 +0100

    made more changes

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const time = document.getElementById("utc-time");
 const day = document.getElementById("day-of-week");
-const barsBtn = document.querySelector(".fa-bars");
-const cancelBtn = document.querySelector(".fa-xmark");
-const dropdownContent = document.querySelector(".drop-down");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const barsBtn = document.querySelector(".fa-bars");
+  const cancelBtn = document.querySelector(".fa-xmark");
+  const dropdownContent = document.querySelector(".drop-down");
+
+  barsBtn.addEventListener("click", function () {
+    dropdownContent.style.display = "block";
+    cancelBtn.style.display = "inline";
+    barsBtn.style.display = "none";
+  });
+
+  cancelBtn.addEventListener("click", function () {
+    dropdownContent.style.display = "none";
+    barsBtn.style.display = "inline";
+    cancelBtn.style.display = "none";
+  });
+
+  // Close the dropdown if the user clicks outside of it
+  window.addEventListener("click", function (event) {
+    if (
+      !event.target.matches(".fa-bars") &&
+      !event.target.matches(".fa-xmark")
+    ) {
+      dropdownContent.style.display = "none";
+      barsBtn.style.display = "inline";
+      cancelBtn.style.display = "none";
+    }
+  });
+});
 
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", function () {
@@ -32,24 +59,3 @@ function updateUTCTime() {
 
 updateUTCTime();
 setInterval(updateUTCTime, 1000);
-
-barsBtn.addEventListener("click", function () {
-  dropdownContent.classList.remove("hide");
-  cancelBtn.classList.remove("hide");
-  barsBtn.classList.add("hide");
-});
-
-cancelBtn.addEventListener("click", function () {
-  dropdownContent.classList.add("hide");
-  barsBtn.classList.remove("hide");
-  cancelBtn.classList.add("hide");
-});
-
-//   Close the dropdown if the user clicks outside of it
-window.addEventListener("click", function (event) {
-  if (!event.target.matches(".fa-bars") && !event.target.matches(".fa-xmark")) {
-    dropdownContent.classList.add("hide");
-    barsBtn.classList.remove("hide");
-    cancelBtn.classList.add("hide");
-  }
-});
