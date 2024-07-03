3323bd74681322eb760d4e9f4566ee08276c1147
YudahTech <dammilare70@gmail.com>
2024-07-03 22:38:55 +0100

    reverted those changes

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const time = document.getElementById("utc-time");
 const day = document.getElementById("day-of-week");
+const barsBtn = document.querySelector(".fa-bars");
+const cancelBtn = document.querySelector(".fa-xmark");
+const dropdownContent = document.querySelector(".drop-down");
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", function () {
+    navLinks.forEach((navLink) => navLink.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+function updateUTCTime() {
+  const now = new Date();
+  const utcTime = now.toUTCString();
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const dayOfWeek = dayNames[now.getUTCDay()];
+
+  time.innerText = `${utcTime}`;
+  day.innerText = `${dayOfWeek}`;
+}
+
+updateUTCTime();
+setInterval(updateUTCTime, 1000);
 
 document.addEventListener("DOMContentLoaded", function () {
   const barsBtn = document.querySelector(".fa-bars");
@@ -31,31 +62,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
-
-navLinks.forEach((navLink) => {
-  navLink.addEventListener("click", function () {
-    navLinks.forEach((navLink) => navLink.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
-
-function updateUTCTime() {
-  const now = new Date();
-  const utcTime = now.toUTCString();
-  const dayNames = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const dayOfWeek = dayNames[now.getUTCDay()];
-
-  time.innerText = `${utcTime}`;
-  day.innerText = `${dayOfWeek}`;
-}
-
-updateUTCTime();
-setInterval(updateUTCTime, 1000);
