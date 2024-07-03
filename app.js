607414725102d1044348fd9abdc6bf55a4e224e6
YudahTech <dammilare70@gmail.com>
2024-07-03 18:57:46 +0100

    made some changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const time = document.getElementById("utc-time");
 const day = document.getElementById("day-of-week");
+const barsBtn = document.querySelector(".fa-bars");
+const cancelBtn = document.querySelector(".fa-xmark");
+const dropdownContent = document.querySelector(".drop-down");
 
 navLinks.forEach((navLink) => {
   navLink.addEventListener("click", function () {
@@ -30,35 +33,23 @@ function updateUTCTime() {
 updateUTCTime();
 setInterval(updateUTCTime, 1000);
 
-document.addEventListener("DOMContentLoaded", function () {
-  const barsBtn = document.querySelector(".fa-bars");
-  const cancelBtn = document.querySelector(".fa-xmark");
-  const dropdownContent = document.querySelector(".drop-down");
+barsBtn.addEventListener("click", function () {
+  dropdownContent.classList.remove("hide");
+  cancelBtn.classList.remove("hide");
+  barsBtn.classList.add("hide");
+});
 
-  barsBtn.addEventListener("click", function () {
-    // dropdownContent.style.bottom = "0";
-    dropdownContent.classList.remove("hide");
-    cancelBtn.classList.remove("hide");
-    barsBtn.classList.add("hide");
-  });
+cancelBtn.addEventListener("click", function () {
+  dropdownContent.classList.add("hide");
+  barsBtn.classList.remove("hide");
+  cancelBtn.classList.add("hide");
+});
 
-  cancelBtn.addEventListener("click", function () {
-    // dropdownContent.style.bottom = "300px";
+//   Close the dropdown if the user clicks outside of it
+window.addEventListener("click", function (event) {
+  if (!event.target.matches(".fa-bars") && !event.target.matches(".fa-xmark")) {
     dropdownContent.classList.add("hide");
     barsBtn.classList.remove("hide");
     cancelBtn.classList.add("hide");
-  });
-
-  //   Close the dropdown if the user clicks outside of it
-  window.addEventListener("click", function (event) {
-    if (
-      !event.target.matches(".fa-bars") &&
-      !event.target.matches(".fa-xmark")
-    ) {
-      //   dropdownContent.style.bottom = "300px";
-      dropdownContent.classList.add("hide");
-      barsBtn.classList.remove("hide");
-      cancelBtn.classList.add("hide");
-    }
-  });
+  }
 });
